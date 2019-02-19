1799c8ba58f04d2bd7502e1d60a92ed8bb720209
xhyzzZ <41977460+xhyzzZ@users.noreply.github.com>
2019-02-19 17:22:19 -0500

    Update AIModule.py

--- ec500 module/AIModule.py
+++ ec500 module/AIModule.py
@@ -1,5 +1,4 @@
 import numpy as np
-import databaseModule
 
 # Input: ID(from main function perhaps), infoDB(from Database function)
 # output: Three predicted parameters, three Alert signals(Type:Boolean
