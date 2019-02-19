e938240f3e89ce3fda70bb14808d65a161d48424
xhyzzZ <41977460+xhyzzZ@users.noreply.github.com>
2019-02-19 17:31:29 -0500

    Update AIModule.py

--- ec500 module/AIModule.py
+++ ec500 module/AIModule.py
@@ -1,61 +1,61 @@
-import numpy as np
+# import numpy as np
 
-# Input: ID(from main function perhaps), infoDB(from Database function)
-# output: Three predicted parameters, three Alert signals(Type:Boolean
+# # Input: ID(from main function perhaps), infoDB(from Database function)
+# # output: Three predicted parameters, three Alert signals(Type:Boolean
 
-class AI_module(object):
-    def __init__(self, dict):
-        self._dict = dict
+# class AI_module(object):
+#     def __init__(self, dict):
+#         self._dict = dict
 
-    def Query_Data_From_Database(self, ID, infoDB):
-        ## connect database, query previous one day data from Database
-        # Database = database_dict()
-        Blood_oxygen = []
-        Blood_pressure = []
-        Pulses = []
+#     def Query_Data_From_Database(self, ID, infoDB):
+#         ## connect database, query previous one day data from Database
+#         # Database = database_dict()
+#         Blood_oxygen = []
+#         Blood_pressure = []
+#         Pulses = []
         
-        # Username = input("")
-        # get dictionary from database
-        for key in infoDB:
-            if key == ID:
-#                 pressure = info['blood_pressure']
-#                 oxygen = info['blood_oxygen']
-#                 Pulse = info['blood_pulses']
-                Blood_pressure.append(pressure)
-                Blood_oxygen.append(oxygen)
-                Pulses.append(Pulse)
+#         # Username = input("")
+#         # get dictionary from database
+#         for key in infoDB:
+#             if key == ID:
+# #                 pressure = info['blood_pressure']
+# #                 oxygen = info['blood_oxygen']
+# #                 Pulse = info['blood_pulses']
+#                 Blood_pressure.append(pressure)
+#                 Blood_oxygen.append(oxygen)
+#                 Pulses.append(Pulse)
 
-        return Blood_oxygen, Blood_pressure, Pulses
+#         return Blood_oxygen, Blood_pressure, Pulses
 
-    def AI_Module(self, Blood_oxygen, Blood_pressure, Pulses):
+#     def AI_Module(self, Blood_oxygen, Blood_pressure, Pulses):
 
-        ## AI module do the prediection, The AI module uses previous data
-        oxygen = np.array(int(Blood_oxygen))
-        pressure = np.array(int(Blood_pressure))
-        Pulse = np.array(int(Pulses))
-        pressure_predict_result = np.mean(int(pressure))
-        oxygen_predict_result = np.mean(int(oxygen))
-        Pulse_predict_result = np.mean(int(Pulse))
+#         ## AI module do the prediection, The AI module uses previous data
+#         oxygen = np.array(int(Blood_oxygen))
+#         pressure = np.array(int(Blood_pressure))
+#         Pulse = np.array(int(Pulses))
+#         pressure_predict_result = np.mean(int(pressure))
+#         oxygen_predict_result = np.mean(int(oxygen))
+#         Pulse_predict_result = np.mean(int(Pulse))
 
-        return pressure_predict_result, oxygen_predict_result, Pulse_predict_result
+#         return pressure_predict_result, oxygen_predict_result, Pulse_predict_result
 
-    def Feedback(self, Blood_pressure_predict_result, Blood_oxygen_predict_result, Pulse_predict_result):
-        lower_BP = 80
-        upper_BP = 120
-        lower_BO = 80
-        upper_BO = 120
-        lower_Pulse = 80
-        upper_Pulse = 120
-        BP_Alert = False
-        BO_Alert = False
+#     def Feedback(self, Blood_pressure_predict_result, Blood_oxygen_predict_result, Pulse_predict_result):
+#         lower_BP = 80
+#         upper_BP = 120
+#         lower_BO = 80
+#         upper_BO = 120
+#         lower_Pulse = 80
+#         upper_Pulse = 120
+#         BP_Alert = False
+#         BO_Alert = False
 
-        Pulse_Alert = False
-        if (Blood_oxygen_predict_result < lower_BO or Blood_oxygen_predict_result > upper_BO):
-            BO_Alert = True
-        if (Blood_pressure_predict_result < lower_BP or Blood_pressure_predict_result > upper_BP):
-            BP_Alert = True
-        if (Pulse_predict_result < lower_Pulse or Pulse_predict_result > upper_Pulse):
-            Pulse_Alert = True
-        ## feedback the AI prediction restult to the interface
-        ## It will turn on the Alert when the statues get worse.
-        return BO_Alert, BP_Alert, Pulse_Alert
+#         Pulse_Alert = False
+#         if (Blood_oxygen_predict_result < lower_BO or Blood_oxygen_predict_result > upper_BO):
+#             BO_Alert = True
+#         if (Blood_pressure_predict_result < lower_BP or Blood_pressure_predict_result > upper_BP):
+#             BP_Alert = True
+#         if (Pulse_predict_result < lower_Pulse or Pulse_predict_result > upper_Pulse):
+#             Pulse_Alert = True
+#         ## feedback the AI prediction restult to the interface
+#         ## It will turn on the Alert when the statues get worse.
+#         return BO_Alert, BP_Alert, Pulse_Alert
